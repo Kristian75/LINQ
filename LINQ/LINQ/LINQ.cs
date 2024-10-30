4ba965c578f0b023b2ac7983a2b8147ee3df54b8
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2024-10-30 08:56:31 +0100

    Add files via upload

---
+++ LINQ/LINQ/LINQ.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>()
+            {
+                "C# Tutorials",
+                "VB.NET Tutorials",
+                "Learn C++",
+                "MVC Tutorials",
+                "Java"
+            };
+
+            /*
+            var result = from s in stringList
+                         where s.Contains("Tutorials")
+                         select s;
+            */
+
+            var result = stringList.Where(s => s.Contains("Tutorials"));
+
+            foreach (var str in result)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>()
+            {
+                "C# Tutorials",
+                "VB.NET Tutorials",
+                "Learn C++",
+                "MVC Tutorials",
+                "Java"
+            };
+            var result = from s in stringList
+                         where s.Contains("Tutorials")
+                         select s;
+
+            foreach (var str in result)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
